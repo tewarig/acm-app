771e07ee4818aec51d057dc69007562ad6badc62
tewarig <gauravtewari@techie.com>
2020-02-04 19:33:47 +0530

    click works

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import {StyleSheet,View,Text} from 'react-native';
-export default class SwitchExample extends Component {
+export default class SwitchExample extends Component
+{
+  state = {
+    myState: "this is frist app"
+  }
+  updateState = () => this.setState({myState:"ohm!!! 1st click"})
     render() {
         return (
             <View style={styles.container}>
                 <View style={{backgroundColor: 'blue', flex: 0.3}} />
                 <View style={{backgroundColor: 'red', flex: 0.5}} />
-                <Text style={{fontSize: 18, flex: 0.2 }}></Text>
+                <Text style={{fontSize: 18, flex: 0.2 }} onPress={this.updateState}>{this.state.myState}</Text>
             </View>
         );
     }
