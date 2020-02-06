2eb2ba4170b3f1842f6624338a98792c1feed8eb
tewarig <gauravtewari@techie.com>
2020-02-06 19:46:05 +0530

    new buttons

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View ,AppRegistry, Dimensions } from "react-native";
 import { DrawerNavigator } from 'react-navigation';
+import FontAwesome, { Icons,IconTypes,parseIconName } from 'react-native-fontawesome';
+
+
 
 import { SliderBox } from "react-native-image-slider-box";
 
 export default class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -23,16 +27,24 @@ export default class App extends Component {
       <View style={styles.container}>
       <Text style={styles.welcome}>GEU ACM </Text>
         <SliderBox
-          images={this.state.images}
-          onCurrentImagePressed={index =>
-            console.warn(`image ${index} pressed`)
-          }
-        />
-       <Text style={styles.bo1}>Forgot Password?</Text>
-        <Text style={styles.bo2}>Forgot Password?</Text>
-         <Text style={styles.bo3}>Forgot Password?</Text>
-          <Text style={styles.bo3}>Forgot Password?</Text>
-           <Text style={styles.bo4}>Forgot Password?</Text>
+          images={this.state.images}        />
+  <View style={{flex: 1, flexDirection: 'row', borderWidth: 2 ,marginTop: '4%'}}>
+      <Text style={styles.squarebox}>about acm</Text>
+      <Text style={styles.squarebox}>about acm</Text>
+      </View>
+      <View style={{flex: 1, flexDirection: 'row', borderWidth: 2 ,marginTop: '5%'}}>
+          <Text style={styles.squarebox}>about acm</Text>
+          <Text style={styles.reactgularbox}>about acm</Text>
+            <Text style={styles.reactgularbox1}>about acm</Text>
+          </View>
+          <View style={{flex: 1, flexDirection: 'row', borderWidth: 2 ,marginTop: '4%'}}>
+              <Text style={styles.reactgularboxd}>about acm</Text>
+              <Text style={styles.reactgularbox1d}>about acm</Text>
+              <Text style={styles.reactgularboxd}>about acm</Text>
+              <Text style={styles.reactgularbox1d}>about acm</Text>
+
+              </View>
+
       </View>
     );
   }
@@ -40,21 +52,89 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1 ,
+    color: '#f3f3f3' ,
+    backgroundColor: '#D6FFFC',
   } ,
   welcome: {
     fontSize: 30,
     left: '30%' ,
   } ,
-  bo1:{
+  squarebox:{
   color:"black",
   fontSize:11 ,
   alignItems:"center",
   justifyContent:"center",
-  padding: 5 ,
-  height: 25 ,
+  padding: 30,
   borderColor: "pink" ,
   borderWidth: 2 ,
+  width: '35%',
+  height: '100%' ,
+  borderRadius: 5,
+  backgroundColor: '#9b59b6' ,
+  textAlign: 'center',
+  marginLeft: '6%' ,
 },
+reactgularbox: {
+  color:"black",
+  fontSize:10 ,
+  alignItems:"center",
+  justifyContent:"center",
+  padding: 5,
+  borderColor: "pink" ,
+  borderWidth: 2 ,
+  width: '15%',
+  height: '100%' ,
+  borderRadius: 9,
+  backgroundColor: '#9b59b6' ,
+  textAlign: 'center',
+  marginLeft: '6%' ,
+},
+reactgularbox1: {
+  color:"black",
+  fontSize:10 ,
+  alignItems:"center",
+  justifyContent:"center",
+  padding: 5,
+  borderColor: "pink" ,
+  borderWidth: 2 ,
+  width: '17%',
+  height: '100%' ,
+  borderRadius: 9,
+  backgroundColor: '#9b59b6' ,
+  textAlign: 'center',
+  marginLeft: '3%' ,
+},
+reactgularboxd: {
+  color:"black",
+  fontSize:10 ,
+  alignItems:"center",
+  justifyContent:"center",
+  padding: 5,
+  borderColor: "pink" ,
+  borderWidth: 2 ,
+  width: '20%',
+  height: '53%' ,
+  borderRadius: 9,
+  backgroundColor: '#9b59b6' ,
+  textAlign: 'center',
+  marginLeft: '2%' ,
+},
+reactgularbox1d: {
+  color:"black",
+  fontSize:10 ,
+  alignItems:"center",
+  justifyContent:"center",
+  padding: 5,
+  borderColor: "pink" ,
+  borderWidth: 2 ,
+  width: '20%',
+  height: '53%' ,
+  borderRadius: 9,
+  backgroundColor: '#9b59b6' ,
+  textAlign: 'center',
+  marginLeft: '2%' ,
+},
+
 
 });
